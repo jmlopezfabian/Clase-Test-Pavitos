41731e5c6336107cd62ac8de1e3b648a318643f1
jmlopezfabian <jmlopezfabian@gmail.com>
2026-04-19 19:17:37 -0600

    Agreando git ignore

--- hola.py
+++ hola.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv # !pip install dotenv
+
 print("Hola mundo")
 
 def saludar():
     print("Hola :D")
+
+clave_super_secreta = os.getenv("llave_api_super_secreta")
