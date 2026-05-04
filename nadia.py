a1857dcef784f527b25fffcc0626cee8ed32829a
NaLi <nadia.lizbeth9@gmail.com>
2026-05-03 19:19:07 -0600

    Inicio de los flow

--- nadia.py
+++ nadia.py
@@ -2,4 +2,8 @@ print("Hola gsu aver si queda animo a la vida")
 
 print("No digo adios por que aca seguiremos")
 
+print("HolaSOynadiafearure")
+
+
+
 #Imaginemos que acabe mi rama pero alguien acabo antesono
